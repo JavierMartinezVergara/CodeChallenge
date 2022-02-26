f983a57469a83f24e766700691789ef25e4f02b8
Javier Martinez <javierrrr.15@gmail.com>
2022-02-26 16:26:29 -0600

    Connect Service Done

---
+++ app/src/main/java/com/javiermtz/codechallenge/data/repository/DogsService.kt
@@ -0,0 +1,35 @@
+package com.javiermtz.codechallenge.data.repository
+
+import com.javiermtz.codechallenge.data.api.DogsApi
+import com.javiermtz.codechallenge.model.response.DogsResponse
+import com.javiermtz.util.ResultWrapper
+import com.javiermtz.util.ResultWrapper.GenericError
+import com.javiermtz.util.ResultWrapper.Success
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import java.lang.Exception
+import javax.inject.Inject
+
+class DogsService @Inject constructor(
+  private val apiService : DogsApi
+) {
+
+
+  suspend fun getDogsFromApi() : ResultWrapper<DogsResponse>{
+    return try {
+      withContext(Dispatchers.IO){
+        val response = apiService.getDogs()
+        if(response.isSuccessful){
+          val body = response.body()
+          ResultWrapper.Success(body!!)
+        } else {
+          GenericError("")
+        }
+      }
+    }catch (e : Exception){
+      GenericError(e.message)
+    }
+
+  }
+}
