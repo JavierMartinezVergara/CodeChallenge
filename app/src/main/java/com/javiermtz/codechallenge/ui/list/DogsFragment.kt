fe5d0cea0aefca47c67160909ec49250eccaa655
Javier Martinez <javierrrr.15@gmail.com>
2022-02-27 17:11:14 -0600

    Set Views and Adapters with Recycler View

---
+++ app/src/main/java/com/javiermtz/codechallenge/ui/list/DogsFragment.kt
@@ -0,0 +1,43 @@
+package com.javiermtz.codechallenge.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.javiermtz.codechallenge.databinding.FragmentDogsBinding
+import com.javiermtz.codechallenge.ui.DogsViewModel
+import com.javiermtz.codechallenge.ui.list.DogsAdapter.OnClickListener
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DogsFragment : Fragment() {
+  private lateinit var binding: FragmentDogsBinding
+  private val viewModel: DogsViewModel by viewModels()
+  private lateinit var adapter: DogsAdapter
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentDogsBinding.inflate(layoutInflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    observers()
+    adapter = DogsAdapter(OnClickListener { dataCharacter ->
+
+    })
+    binding.recyclerViewCharacters.adapter = adapter
+
+  }
+
+  private fun observers() {
+    viewModel.data.observe(viewLifecycleOwner, {
+      adapter.submitList(it)
+    })
+  }
+}
